842b0cfc0f6a6e38de1d42852254e6995f73952c
AlejandroCasanova000 <75802105+AlejandroCasanova000@users.noreply.github.com>
2021-01-27 15:53:57 +0100

    Comentarios

--- src/BiziZgz/Programa2ContarUsos.java
+++ src/BiziZgz/Programa2ContarUsos.java
@@ -5,7 +5,18 @@ import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.Scanner;
 
+/**
+ * Esta clase Java, dado el path de un fichero .csv con la estructura determinada, muestra el numero totales de UsosBizi y la
+ * el numero de recorridos si son Usos Circulares (RetiroEstacion y AnclajeEstacion son iguales), o si son traslados.
+ * @author alexc
+ *
+ */
 public class Programa2ContarUsos {
+	
+	/**
+	 * Este metodo unicamente pregunta al usuario por un path, y llama a los metodos necesarios.
+	 * @param args
+	 */
 	public static void main(String[] args) {
 		Scanner entrada = new Scanner(System.in);
 		String path = "";
@@ -18,6 +29,13 @@ public class Programa2ContarUsos {
 		entrada.close();
 	}
 	
+	/**
+	 * Pre: Este metodo dado un path de un fichero .csv, asigna UsosBizi a cada línea del fichero
+	 * Post: Para ello se recoore cada linea del fichero creando un UsoBBizi a cada una de ellas con sus respectivos
+	 * atributos.
+	 * @param path Ruta del fichero
+	 * @return
+	 */
 	public static ArrayList<UsoBizi> mostrarInformacionResumida(String path) {
 		UsoBizi uso;
 		ArrayList<UsoBizi> usos = new ArrayList<UsoBizi>();
@@ -43,6 +61,12 @@ public class Programa2ContarUsos {
 		}
 	}
 	
+	/**
+	 * Pre: Este metodo, dado una lista de UsosBizi [usos], analiza el numero total de recorridos circulares o traslados
+	 * Post: Para ello se recorre la lista y si coinciden las estaciones de anclaje y retiro se suma uno al cCirculos, si no
+	 * se suma uno al cTraslados
+	 * @param usos Lista de usos
+	 */
 	public static void analizarRecorridos (ArrayList<UsoBizi> usos) {
 		int cTraslados = 0;
 		int cCirculos = 0;
@@ -59,6 +83,14 @@ public class Programa2ContarUsos {
 
 	}
 	
+	/**
+	 * Pre: Este metodo devuelve true si un string se puede pasar a int, si no, devuelve false
+	 * Post: Para ello se utiliza el try y catch, se intenta pasar el string [respuesta] a int haciendo uso
+	 * del metodo java Integer.parseInt(), si este metodo devuelve el error java.lang.NumberFormatException, el catch
+	 * devolvera flase, si no salta ningun error, devuelve true.
+	 * @param respuesta
+	 * @return
+	 */
 	public static boolean isInteger (String respuesta) {
 		try {
 			int numero = Integer.parseInt(respuesta);
